a3e4a3535db916c9455b890eaa90ebd0137c5660
JonaStark <80177184+JonaStark@users.noreply.github.com>
2021-04-19 13:30:18 -0600

    Update Stock.java

--- src/main/java/wsStockMarket/Stock.java
+++ src/main/java/wsStockMarket/Stock.java
@@ -1,8 +1,33 @@
 package wsStockMarket;
 
+import java.util.ArrayList;
+
 public class Stock implements Subject {
+	String tipo; // Nombre del stock
+	StockMarket mercado_acciones;
+	ArrayList<Observer> observadores;
+	double precio;
 
 	public Stock(String tipo, double precio_inicial, StockMarket mercado_acciones) {
+		observadores = new ArrayList<>();
+		this.mercado_acciones = mercado_acciones;
+		this.precio = precio_inicial;
+		this.tipo = tipo;
+
 	}
-    
+
+	public void registroObservadores(Observer o) {
+		observadores.add(o);
+	}
+
+	public String notificarObservdores(Observer comerciante, String tipo_operacion, Double precio) {
+		String transaccionlog="";
+		if (comerciante instanceof Trader) {
+			for (Observer observer : observadores) {
+				transaccionlog += observer.getName() + ":The latest trade is Trader:"+ comerciante.getName()+" "
+						+tipo_operacion +" $"+precio+ " Stock: "+ this.tipo+"\n";
+			}
+		}
+		return transaccionlog.substring(0,transaccionlog.length()- 1);
+	}
 }
