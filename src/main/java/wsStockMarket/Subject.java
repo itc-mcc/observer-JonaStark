12af06616993104e0674291407778c9f7ad6dd04
JonaStark <80177184+JonaStark@users.noreply.github.com>
2021-04-19 13:32:06 -0600

    Update Subject.java

--- src/main/java/wsStockMarket/Subject.java
+++ src/main/java/wsStockMarket/Subject.java
@@ -1,5 +1,6 @@
 package wsStockMarket;
 
 public interface Subject {
-
+    void registroObservadores(Observer o);
+    String notificarObservdores(Observer t, String tipo_transaccion, Double precio);
 }
